cdbd89c0d49e0a19eaff992245abf6764b4a02e4
Ashish <ashishnagar1221@gmail.com>
2020-04-20 04:14:26 +0530

    test3 working test 2 still pending

--- src/components/Test/test2.js
+++ src/components/Test/test2.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React,{useState} from 'react';
+import {Link,useHistory} from 'react-router-dom'
 
 /**
 * @author
@@ -6,8 +7,35 @@ import React from 'react'
 **/
 
 const Test2 = () => {
+
+  const history = useHistory();
+  const [user,setUser] = useState("");
+  const [password,setPassword] = useState("");
+
+  const PostData = () =>{
+
+    console.log(user+ " "+ password)
+    
+    
+  }
+
+
   return(
     <div>to check the valid user = pass user_name and password as a query parameter and check if the user exists
+      <input type="text"
+            placeholder ="user name"
+            value ={user}
+            onChange = {(e)=>setUser(e.target.value)}
+            />
+        <input type="password"
+            placeholder = "password"
+            value ={password}
+            onChange = {(e)=>setPassword(e.target.value)}
+            />  
+        <button className="btn waves-effect wave-light"
+        onClick = {()=>PostData()}>
+            Check 
+        </button>
    </div>
    )
 
--- src/components/Test/test3.js
+++ src/components/Test/test3.js
@@ -1,13 +1,57 @@
-import React from 'react'
-
-/**
-* @author
-* @function Test3
-**/
+import React,{useState} from 'react';
+import {useHistory} from 'react-router-dom';
+import M from 'materialize-css'
 
 const Test3 = () => {
+
+  const history = useHistory();
+  const [user,setUser] = useState("");
+  const [title,setTitle] = useState("");
+  const [postText,setPostText] = useState("");
+
+  const PostData = () =>{
+    fetch("/newpost",{
+      method:"post",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify({
+        postedBy:user,
+        title:title,
+        postText:postText
+      })
+    }).then(res => res.json())
+    .then(data => {
+      if(data.error){
+        return data.error
+      }else{
+        M.toast({html:"Posted succesfully"})
+        history.push('/')
+      }
+    })
+  }
+
   return(
     <div> add a blog post = add new blog corresponding to the username in the DB
+      <input type="text"
+            placeholder ="user name"
+            value ={user}
+            onChange = {(e)=>setUser(e.target.value)}
+            />
+        <input type="text"
+            placeholder = "title"
+            value ={title}
+            onChange = {(e)=>setTitle(e.target.value)}
+            />  
+        <input type="text"
+            placeholder = "Body"
+            value ={postText}
+            onChange = {(e)=>setPostText(e.target.value)}
+            />  
+        <button className="btn waves-effect wave-light"
+        onClick = {()=>PostData()}>
+            Check 
+        </button>
    </div>
    )
 
